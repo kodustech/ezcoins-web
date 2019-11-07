b6df3bda650c85b563020456032a1bc335875dd6
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-07 21:42:24 -0200

    Configured apollo graphql

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import './App.css';
 
 import Routes from './router/routes';
 import WithThemeProvider from './hocs/WithThemeProvider';
+import useApi from './store/api';
 
 function App() {
+  const api = useApi();
+
   return (
-    <WithThemeProvider>
-      <Routes />
-    </WithThemeProvider>
+    api && (
+      <ApolloProvider client={api}>
+        <WithThemeProvider>
+          <Routes />
+        </WithThemeProvider>
+      </ApolloProvider>
+    )
   );
 }
 
---
+++ src/store/api.js
@@ -0,0 +1,70 @@
+import { useCallback, useEffect, useState } from 'react';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { RetryLink } from 'apollo-link-retry';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+import resolvers from './resolvers';
+
+export default () => {
+  const [api, setApi] = useState(null);
+
+  const setup = useCallback(async () => {
+    const authLink = setContext(async (_, { headers }) => ({
+      headers: {
+        ...headers,
+      },
+    }));
+
+    const retryLink = new RetryLink();
+    const httpLink = createHttpLink({
+      uri: `https://5a6ac1f2.ngrok.io`,
+    });
+
+    const wsClient = new SubscriptionClient(`wss://5a6ac1f2.ngrok.io`, {
+      reconnect: true,
+    });
+    const webSocketLink = new WebSocketLink(wsClient);
+    const requestLink = ApolloLink.split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+      },
+      webSocketLink,
+      httpLink,
+    );
+
+    const link = ApolloLink.from([authLink, retryLink, requestLink]);
+
+    const cache = new InMemoryCache();
+
+    const data = {
+      isSearchBarVisible: false,
+    };
+
+    cache.writeData({ data });
+
+    const client = new ApolloClient({
+      cache,
+      link,
+      resolvers,
+    });
+
+    client.onResetStore(() => {
+      cache.writeData({ data });
+    });
+
+    setApi(client);
+  }, []);
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  return api;
+};
