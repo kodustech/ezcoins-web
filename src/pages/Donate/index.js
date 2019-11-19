3ad292413db0f3a5c2d3b330a3e1d7308eba0c87
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-19 07:31:13 -0200

    Styled donate card

--- src/pages/Donate/index.js
+++ src/pages/Donate/index.js
@@ -1,17 +1,64 @@
 import React, { memo } from 'react';
-import { Avatar, Container, CssBaseline, FormControl } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import {
+  Avatar,
+  Container,
+  CssBaseline,
+  FormControl,
+  Grid,
+  GridList,
+  GridListTile,
+  InputAdornment,
+  MenuItem,
+  OutlinedInput,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import { KeyboardDatePicker } from '@material-ui/pickers';
-
 import moment from 'moment';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Input from '@material-ui/core/Input';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import avatar from '../../assets/images/avatar.jpeg';
+
+import image from '../../assets/images/bg-login.png';
 import useStyles from './useStyles';
+import avatar from '../../assets/images/avatar.jpeg';
+
+const tileData = [
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+  {
+    img: image,
+    title: 'title',
+    author: 'author',
+  },
+];
 
 const Donate = memo(() => {
   const classes = useStyles();
@@ -103,52 +150,103 @@ const Donate = memo(() => {
             <Avatar src={avatar} className={classes.avatar} />
           </Grid>
         </Grid>
-        <Grid container justify="center" style={{ marginTop: 100 }}>
-          <Grid container xs={5} component={Paper} style={{ borderRadius: 16 }}>
-            <Grid
-              xs={1}
-              style={{
-                height: 150,
-                backgroundColor: 'red',
-                borderTopLeftRadius: 16,
-                borderBottomLeftRadius: 16,
-              }}
-            />
-            <Grid
-              container
-              xs={11}
-              style={{
-                height: 150,
-                backgroundColor: 'blue',
-                borderTopRightRadius: 16,
-                borderBottomRightRadius: 16,
-                padding: 10,
-              }}
-            >
-              <Grid xs={6} component={Typography} variant="h5">
+        <GridList className={classes.gridList} cols={2.1}>
+          {tileData.map((_, index) => (
+            <GridListTile key={index.toString()} style={{ height: 'auto' }}>
+              <Grid container fullWidth component={Paper} style={{ borderRadius: 16 }}>
+                <Grid xs={1} className={classes.edge} />
+                <Grid container xs={11} className={classes.body}>
+                  <Grid xs={7} component={Typography} variant="h4">
+                    Parabéns
+                  </Grid>
+                  <Grid
+                    xs={5}
+                    component={KeyboardDatePicker}
+                    id="date-picker-dialog"
+                    format="DD/MM/YYYY"
+                    value={moment()}
+                    onChange={() => {}}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+                  <Grid
+                    xs={3}
+                    component={Typography}
+                    variant="subtitle1"
+                    className={classes.formRoot}
+                  >
+                    Para:
+                  </Grid>
+                  <Grid xs={9} component={FormControl} className={classes.formRoot}>
+                    <Select value={null} onChange={() => {}} variant="outlined" displayEmpty>
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </Grid>
+                  <Grid
+                    xs={3}
+                    component={Typography}
+                    variant="subtitle1"
+                    className={classes.formRoot}
+                  >
+                    Motivo:
+                  </Grid>
+                  <Grid component={FormControl} xs={9} className={classes.formRoot}>
+                    <Typography variant="body1">
+                      <TextField
+                        id="standard-adornment-amount"
+                        variant="outlined"
+                        multiline
+                        rows={2}
+                        className={classes.multiline}
+                        value="O cara fez uma parada tão da hora que eu vou escrever um texto de agradecimento inclusive vou dar uma aumento"
+                        onChange={() => {}}
+                      />
+                    </Typography>
+                  </Grid>
+                  <Grid xs={9} />
+                  <Grid component={FormControl} xs={3} className={classes.formRoot}>
+                    <OutlinedInput
+                      id="standard-adornment-amount"
+                      value={100}
+                      onChange={() => {}}
+                      startAdornment={<InputAdornment position="start">EZȻ</InputAdornment>}
+                      labelWidth={60}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </GridListTile>
+          ))}
+        </GridList>
+        <Grid container justify="center">
+          <Grid container xs={6} component={Paper} style={{ borderRadius: 16 }}>
+            <Grid xs={1} className={classes.edge} />
+            <Grid container xs={11} className={classes.body}>
+              <Grid xs={7} component={Typography} variant="h4">
                 Parabéns
               </Grid>
               <Grid
-                xs={6}
+                xs={5}
                 component={KeyboardDatePicker}
                 id="date-picker-dialog"
                 format="DD/MM/YYYY"
                 value={moment()}
-                onChange={date => console.log(date)}
+                onChange={() => {}}
                 KeyboardButtonProps={{
                   'aria-label': 'change date',
                 }}
               />
-              <Grid xs={6} component={Typography} variant="subtitle1">
-                Para
+              <Grid xs={3} component={Typography} variant="subtitle1" className={classes.formRoot}>
+                Para:
               </Grid>
-              <Grid xs={6} component={FormControl} className={classes.formControl}>
-                <Select
-                  value={null}
-                  onChange={age => console.log(age)}
-                  displayEmpty
-                  className={classes.selectEmpty}
-                >
+              <Grid xs={9} component={FormControl} className={classes.formRoot}>
+                <Select value={null} onChange={() => {}} variant="outlined" displayEmpty>
                   <MenuItem value="">
                     <em>None</em>
                   </MenuItem>
@@ -157,13 +255,30 @@ const Donate = memo(() => {
                   <MenuItem value={30}>Thirty</MenuItem>
                 </Select>
               </Grid>
-              <Grid xs={6} />
-              <Grid component={FormControl} xs={6} className={classes.margin}>
-                <Input
+              <Grid xs={3} component={Typography} variant="subtitle1" className={classes.formRoot}>
+                Motivo:
+              </Grid>
+              <Grid component={FormControl} xs={9} className={classes.formRoot}>
+                <Typography variant="body1">
+                  <TextField
+                    id="standard-adornment-amount"
+                    variant="outlined"
+                    multiline
+                    rows={2}
+                    className={classes.multiline}
+                    value="O cara fez uma parada tão da hora que eu vou escrever um texto de agradecimento inclusive vou dar uma aumento"
+                    onChange={() => {}}
+                  />
+                </Typography>
+              </Grid>
+              <Grid xs={9} />
+              <Grid component={FormControl} xs={3} className={classes.formRoot}>
+                <OutlinedInput
                   id="standard-adornment-amount"
                   value={100}
                   onChange={() => {}}
                   startAdornment={<InputAdornment position="start">EZȻ</InputAdornment>}
+                  labelWidth={60}
                 />
               </Grid>
             </Grid>
--- src/pages/Donate/useStyles.js
+++ src/pages/Donate/useStyles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 
-export default makeStyles(() => ({
+export default makeStyles(theme => ({
   avatar: {
     margin: 10,
     height: 80,
@@ -8,5 +8,45 @@ export default makeStyles(() => ({
   },
   container: {
     marginTop: 15,
+    marginBottom: 50,
+  },
+  edge: {
+    backgroundColor: theme.palette.primary.dark,
+    borderTopLeftRadius: 16,
+    borderBottomLeftRadius: 16,
+  },
+  body: {
+    backgroundColor: theme.palette.primary.light,
+    borderTopRightRadius: 16,
+    borderBottomRightRadius: 16,
+    padding: 10,
+    color: 'white',
+  },
+  multiline: {
+    width: '100%',
+  },
+  formRoot: {
+    marginTop: 10,
+  },
+
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    height: 300,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
   },
 }));
