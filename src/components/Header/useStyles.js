e98ca34314838d2a0bffd67f56369fe06d01263b
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-20 10:40:30 -0200

    Removed unnecessary code and changed background color

--- src/components/Header/useStyles.js
+++ src/components/Header/useStyles.js
@@ -5,49 +5,15 @@ export default makeStyles(theme => ({
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  profile: {
+    height: 40,
+    width: 50,
   },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
+  profileContainer: {
+    alignContent: 'center',
+    backgroundColor: fade(theme.palette.primary.light, 0.5),
+    height: 55,
+    marginLeft: 20,
   },
   sectionDesktop: {
     display: 'none',
@@ -61,13 +27,10 @@ export default makeStyles(theme => ({
       display: 'none',
     },
   },
-  profileContainer: {
-    marginLeft: 20,
-    height: 55,
-    alignContent: 'center',
-  },
-  profile: {
-    height: 40,
-    width: 50,
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   },
 }));
