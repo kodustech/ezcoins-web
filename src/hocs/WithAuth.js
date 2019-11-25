a0197e12faa63b70896e000d2a9d9c01788b7131
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-25 12:40:32 -0200

    Bug fixed header authentication

--- src/hocs/WithAuth.js
+++ src/hocs/WithAuth.js
@@ -4,7 +4,9 @@ import { useLocation } from 'react-router-dom';
 export default ({ children }) => {
   const { pathname } = useLocation();
 
-  const isAuthenticated = useMemo(() => pathname !== '/login', [pathname]);
+  const isAuthenticated = useMemo(() => pathname !== '/login' && localStorage.getItem('token'), [
+    pathname,
+  ]);
 
   return isAuthenticated ? children : null;
 };
