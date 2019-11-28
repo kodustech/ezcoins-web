263a596365b7bb1b2e21aaf4b11f362d1c7e072c
maykon@deepin <maykonmichelpalma@gmail.com>
2019-11-28 15:02:37 -0200

    Added no cash error

--- src/pages/Donate/index.js
+++ src/pages/Donate/index.js
@@ -4,10 +4,25 @@ import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import Lottie from 'react-lottie';
 
+import Typography from '@material-ui/core/Typography';
 import useForm from './useForm';
-import boxLottieFile from '../../assets/lottiefiles/8367-open-box';
+import openBox from '../../assets/lottiefiles/8367-open-box';
+import emptyBox from '../../assets/lottiefiles/629-empty-box';
 import AvatarSelect from '../../components/AvatarSelect';
 import DonationCard from '../../components/DonationCard';
+import useStyles from './useStyles';
+
+const USER = gql`
+  query($id: ID) {
+    user(id: $id) {
+      id
+      wallet {
+        id
+        toOffer
+      }
+    }
+  }
+`;
 
 const USERS = gql`
   query {
@@ -20,16 +35,38 @@ const USERS = gql`
 `;
 
 const Donate = memo(() => {
+  const classes = useStyles();
+
   const animation = useRef(null);
   const container = useRef(null);
 
   const { data: { users = [] } = {}, error, loading } = useQuery(USERS);
+  const { data: { user: { wallet: { toOffer } = {} } = {} } = {} } = useQuery(USER, {
+    variables: { id: localStorage.getItem('userId') },
+    fetchPolicy: 'cache-only',
+  });
 
   const form = useForm();
 
   if (loading) return <div>Loading</div>;
 
   if (error) return <div>Erro</div>;
+
+  if (!toOffer)
+    return (
+      <Grid container justify="center">
+        <Typography variant="h5" align="center" className={classes.title}>
+          Você já utilizou todos os ezcoins para doar este mês :(
+        </Typography>
+        <Lottie
+          options={{
+            animationData: emptyBox,
+          }}
+          height={500}
+          isClickToPauseDisabled
+        />
+      </Grid>
+    );
 
   return (
     <>
@@ -59,7 +96,7 @@ const Donate = memo(() => {
             options={{
               autoplay: false,
               loop: false,
-              animationData: boxLottieFile,
+              animationData: openBox,
             }}
             style={{
               height: 600,
---
+++ src/pages/Donate/useStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles(theme => ({
+  title: {
+    marginTop: 100,
+    marginBottom: 50,
+    color: theme.palette.primary.dark,
+    fontWeight: theme.typography.fontWeightBold,
+  },
+}));
