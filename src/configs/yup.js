57bc6b295bddfccfacb5d0cf4093dd4fa282e2c0
maykonmichel <maykonmichelpalma@gmail.com>
2019-12-12 10:57:05 -0200

    Added max and min yup translations messages

--- src/configs/yup.js
+++ src/configs/yup.js
@@ -1,12 +1,23 @@
 import { setLocale } from 'yup';
+import { map } from 'ramda';
+import { isDate, format } from 'date-fns';
 import i18next from './locales';
 
+const formatDates = map(v => (isDate(v) ? format(v, 'dd/MM/yyyy') : v));
+
 setLocale({
+  date: {
+    min: vars => i18next.t('date.min', formatDates(vars)),
+    max: vars => i18next.t('date.max', formatDates(vars)),
+  },
   mixed: {
     default: vars => i18next.t('mixed.default', vars),
     required: vars => i18next.t('mixed.required', vars),
     oneOf: vars => i18next.t('mixed.oneOf', vars),
     notOneOf: vars => i18next.t('mixed.  notOneOf', vars),
+  },
+  number: {
+    moreThan: vars => i18next.t('number.moreThan', vars),
   },
   string: {
     length: vars => i18next.t('string.length', vars),
@@ -18,8 +29,5 @@ setLocale({
     trim: vars => i18next.t('string.trim', vars),
     lowercase: vars => i18next.t('string.lowercase', vars),
     uppercase: vars => i18next.t('string.uppercase', vars),
-  },
-  number: {
-    moreThan: vars => i18next.t('number.moreThan', vars),
   },
 });
