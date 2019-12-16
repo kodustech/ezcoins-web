840eb7279194fbbc747574afdf53924149c8ecd4
maykonmichel <maykonmichelpalma@gmail.com>
2019-12-16 15:58:06 -0200

    Fix donation date

--- src/components/DonationCard/useStyles.js
+++ src/components/DonationCard/useStyles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
 
 export default makeStyles(theme => ({
   body: {
@@ -17,6 +18,15 @@ export default makeStyles(theme => ({
     backgroundColor: theme.palette.primary.dark,
     borderTopLeftRadius: 16,
     borderBottomLeftRadius: 16,
+  },
+  insertedAt: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    color: grey['700'],
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 38,
   },
   multiline: {
     width: '100%',
--- src/pages/Donate/useForm.js
+++ src/pages/Donate/useForm.js
@@ -88,7 +88,6 @@ export default () => {
   const initialValues = useMemo(
     () => ({
       receiverUserId: '',
-      insertedAt: new Date().toISOString(),
       reason: '',
       quantity: 0,
     }),
@@ -98,9 +97,6 @@ export default () => {
   const validationSchema = Yup.object().shape({
     receiverUserId: Yup.string()
       .label('Destinatário')
-      .required(),
-    insertedAt: Yup.date()
-      .label('Data')
       .required(),
     reason: Yup.string()
       .min(5)
